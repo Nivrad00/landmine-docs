02a362571c8b4f123c5fff3ff73b2fa55f778bed
Darvin <darvinheo@gmail.com>
2021-02-19 16:09:41 -0500

    end of sprint one (plus dynamic loading of notes!)

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
 	$('.meetingNotes').css('display', 'none');
 	$('.meetingTitle').on('click', toggleVisible);
+	
+	$.get({
+	  url: "meeting-notes.csv",
+	  dataType: "text",       
+	  success: response => {
+		let data = $.csv.toArrays(response);
+		generateMeetingNotes(data);
+	  }   
+	});
 });
 
+function generateMeetingNotes(data) {
+	for (let line of data) {
+		if (line[0].length > 0)
+			$(`<h3>${line[0]}</h3>`).appendTo('meetingLog')
+		
+		$("<div/>", {
+			"class": "meetingTitle",
+			text: line[1]
+		}).appendTo('meetingLog');
+		
+		$("<div/>", {
+			"class": "meetingNotes",
+			text: line[2]
+		}).appendTo('meetingLog');
+	}
+}
+	
 function toggleVisible(e) {
 	var notes = $(e.currentTarget).next('.meetingNotes');
 	if (notes.css('display') == "none") {
